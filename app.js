1eb4f2ca31e74fda741e7915776add72cce4b245
Thanvithmk <thanvithmk@gmail.com>
2025-07-31 10:40:43 +0530

    added edit,delete and new routes and ejs

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
 
+//method override
+const methodOverride=require('method-override');
+app.use(methodOverride('_method'));
+
 //routes
 app.get('/',(req,res)=>{
     res.send('Hello World');
@@ -30,12 +34,48 @@ app.get('/listings',async(req,res)=>{
     res.render('listings/index.ejs',{allListings})
 })
 
+//new route
+app.get('/listings/new',(req,res)=>{
+    res.render('listings/new.ejs')
+})
+
+//create route
+app.post('/listings',async(req,res)=>{
+    let listing=req.body.listing;
+    await new Listing(listing).save();
+    res.redirect('/listings');
+})
+
+//edit route
+app.get("/listings/:id/edit",async(req,res)=>{
+    const {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render('listings/edit.ejs',{listing})
+})
+
+//update route
+app.put("/listings/:id",async(req,res)=>{
+    const {id}=req.params;
+    const listing=await Listing.findByIdAndUpdate(id,req.body.listing,{new:true});
+    res.redirect(`/listings/${listing._id}`);
+})
+
 //show route
 app.get('/listings/:id',async(req,res)=>{
     const {id}=req.params;
     const listing=await Listing.findById(id);
     res.render('listings/show.ejs',{listing})
 });
+
+//delete route
+app.delete("/listings/:id",async(req,res)=>{
+    const {id}=req.params;
+    await Listing.findByIdAndDelete(id);
+    res.redirect('/listings');
+})
+
+
+
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
