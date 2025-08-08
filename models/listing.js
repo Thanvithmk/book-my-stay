6f6c74858094ddc1e510d3faa7da94f646962675
Thanvithmk <thanvithmk@gmail.com>
2025-08-08 09:22:18 +0530

    deleted all reviews when a listing is deleted

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose');
 const Schema=mongoose.Schema;
+const Review=require('./review');
 
 //schema
 const listingSchema=new Schema({
@@ -39,6 +40,13 @@ const listingSchema=new Schema({
         }
     ]
 });
+
+//delete all reviews when a listing is deleted
+listingSchema.post('findOneAndDelete',async function(listing){
+    if(listing){
+        await Review.deleteMany({_id:{$in:listing.reviews}});
+    }
+})
 
 //model
 const Listing=mongoose.model('Listing',listingSchema);
