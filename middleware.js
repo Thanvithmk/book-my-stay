e4c415c81aeb7c94a8c8484d04c7492d1bb21cee
Thanvithmk <thanvithmk@gmail.com>
2025-08-09 20:40:55 +0530

    Post login redirect to the url the user was trying to access

--- middleware.js
+++ middleware.js
@@ -1,7 +1,16 @@
 module.exports.isLoggedIn=(req,res,next)=>{
     if(!req.isAuthenticated()){
+        req.session.redirectUrl=req.originalUrl; //code_id :thanv2 store the url the user is trying to access
         req.flash('error','You must be logged in to create a listing');
         return res.redirect('/login');
+    }
+    next();
+}
+
+//code_id :thanv2.1 to prevent passport from changing originalUrl during login
+module.exports.saveRedirectUrl=(req,res,next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl=req.session.redirectUrl;
     }
     next();
 }
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const User=require("../models/user"); //model for user
 const wrapAsync=require('../utils/warpAsync'); //to handle async errors in routes using try catch block 
 const passport=require("passport");
 const {isLoggedIn}=require("../middleware");
+const {saveRedirectUrl}=require("../middleware");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup"); //render the signup form
@@ -14,8 +15,13 @@ router.post("/signup",wrapAsync(async(req,res)=>{
         let {username,email,password}=req.body;
         const newUser=new User({username,email});
         const registeredUser=await User.register(newUser,password);
-        req.flash("success","Welcome to Wanderlust");
-        res.redirect("/listings");
+        req.login(registeredUser,(err)=>{ //id : thanv1 login the user after signup automatically
+            if(err){
+                return next(err);
+            }
+            req.flash("success","Welcome to Wanderlust");    
+            res.redirect("/listings");
+        })
     }catch(e){
         req.flash("error",e.message);
         res.redirect("/signup");
@@ -27,9 +33,9 @@ router.get("/login",(req,res)=>{
     res.render("users/login");
 });
 
-router.post("/login",passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),(req,res)=>{
+router.post("/login",saveRedirectUrl,passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),(req,res)=>{
     req.flash("success","Welcome back");
-    res.redirect("/listings");
+    res.redirect(res.locals.redirectUrl || "/listings"); //code_id :thanv2.2 redirect to the url the user was trying to access
 });
 
 router.get("/logout",isLoggedIn,(req,res,next)=>{
