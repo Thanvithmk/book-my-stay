057ef2a8124998b462cc7a00fc6332a929c77720
Thanvithmk <thanvithmk@gmail.com>
2025-08-09 12:31:03 +0530

    Added login route in user.js and updated footer

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express=require("express");
 const router=express.Router({});
 const User=require("../models/user"); //model for user
 const wrapAsync=require('../utils/warpAsync'); //to handle async errors in routes using try catch block 
+const passport=require("passport");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup"); //render the signup form
@@ -12,15 +13,22 @@ router.post("/signup",wrapAsync(async(req,res)=>{
         let {username,email,password}=req.body;
         const newUser=new User({username,email});
         const registeredUser=await User.register(newUser,password);
-        console.log(registeredUser);
         req.flash("success","Welcome to Wanderlust");
         res.redirect("/listings");
     }catch(e){
         req.flash("error",e.message);
-        console.log(e);
         res.redirect("/signup");
     }
 })
 );
+
+router.get("/login",(req,res)=>{
+    res.render("users/login");
+});
+
+router.post("/login",passport.authenticate("local",{failureRedirect:"/login",failureFlash:true}),(req,res)=>{
+    req.flash("success","Welcome back");
+    res.redirect("/listings");
+});
 
 module.exports=router;
